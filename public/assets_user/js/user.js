2036749b2b26a50207b7eaa66b4a4bb4c0a3d039
krishna prasad <krishnaprasadc45@gmail.com>
2023-07-31 23:22:15 +0530

    total price feature added in the cartview page

--- public/assets_user/js/user.js
+++ public/assets_user/js/user.js
@@ -56,3 +56,48 @@ const addToCart = async (productId) => {
         // You can add code here to handle the error, show an error message, or perform any other necessary actions.
     }
 };
+
+
+const totalPrice = async (id, act, stock) => {
+    console.log(11);
+    const elem = document.getElementById(id);
+    
+    if (act == "inc") elem.value ? (elem.value = Number(elem.value) + 1) : "";
+    else if (act == "dec") elem.value > 1 ? (elem.value = Number(elem.value) - 1) : "";
+
+    let subTotal = 0;
+    let datas = [];
+    let length = document.getElementsByName("productTotal").length;
+    console.log(length);
+    
+    for (let i = 0; i < length; i++) {
+        
+        const quantity = parseFloat(document.getElementsByName("num-product")[i].value);
+       
+        const price = parseFloat(document.getElementsByName("productprice")[i].value);
+    
+        const productTotal = isNaN(quantity) || isNaN(price) ? 0 : quantity * price;
+        document.getElementsByName("productTotal")[i].innerText = "₹ " + productTotal.toFixed();
+        subTotal += productTotal;
+      
+
+        datas.push({
+            id: document.getElementsByName("productId")[i].value,
+            quantity: Number(document.getElementsByName("num-product")[i].value),
+        });
+    }
+    // console.log(document.getElementById("subTotal")); 
+    console.log(subTotal);
+    document.getElementById("subTotal").innerText = "₹ " + subTotal.toFixed();
+    document.getElementById("subTotal2").innerText = "₹ " + subTotal.toFixed();
+    console.log(33);
+    let data = await fetch("/cartUpdation", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            datas,
+        }),
+    });
+};
