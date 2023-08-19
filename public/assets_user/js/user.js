5ceca1f37aa50d3bef4a5834efa4e22082640673
krishna prasad <krishnaprasadc45@gmail.com>
2023-08-19 16:46:49 +0530

    code save

--- public/assets_user/js/user.js
+++ public/assets_user/js/user.js
@@ -76,53 +76,53 @@ if (userEditAddress) {
 // user address edit
 
 
-const addToCart = async (productId) => {
+// const addToCart = async (productId) => {
     
-    try {
-    event.preventDefault();
-        const addToCartButton = document.getElementById("addToCartBtn");
-        let quantity = document.getElementById(productId).value;
+//     try {
+//     event.preventDefault();
+//         const addToCartButton = document.getElementById("addToCartBtn");
+//         let quantity = document.getElementById(productId).value;
         
-        if(quantity === null){
-            quantity = 1
-        }
+//         if(quantity === null){
+//             quantity = 1
+//         }
         
         
 
-        const response = await fetch(`/addToCart?id=${productId}&quantity=${quantity}`, {
+//         const response = await fetch(`/addToCart?id=${productId}&quantity=${quantity}`, {
 
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         });
         
-        let data = await response.json();
+//         let data = await response.json();
 
 
 
-        if (data.message === "Item already in cart!!") {
-            Swal.fire({
-                position: "center",
-                icon: "success",
-                title: "Product is already in cart.\n So quantity increased",
-                showConfirmButton: true,
-                confirmButtonColor: "#79a206",
-            });
-        } else {
-            Swal.fire({
-                position: "center",
-                icon: "success",
-                title: "Product successfully added to cart",
-                showConfirmButton: true,
-                confirmButtonColor: "#79a206",
-            });
-        }
-    } catch (error) {
-        console.error("An error occurred:", error);
-        // You can add code here to handle the error, show an error message, or perform any other necessary actions.
-    }
-};
+//         if (data.message === "Item already in cart!!") {
+//             Swal.fire({
+//                 position: "center",
+//                 icon: "success",
+//                 title: "Product is already in cart.\n So quantity increased",
+//                 showConfirmButton: true,
+//                 confirmButtonColor: "#79a206",
+//             });
+//         } else {
+//             Swal.fire({
+//                 position: "center",
+//                 icon: "success",
+//                 title: "Product successfully added to cart",
+//                 showConfirmButton: true,
+//                 confirmButtonColor: "#79a206",
+//             });
+//         }
+//     } catch (error) {
+//         console.error("An error occurred:", error);
+//         // You can add code here to handle the error, show an error message, or perform any other necessary actions.
+//     }
+// };
 const moveToCart = async (productId) => {
 
     try {
